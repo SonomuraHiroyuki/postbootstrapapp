0f6841162c30ed6819a280f18b44255ad5bb33b1
SonomuraHiroyuki <abc332554331@gmail.com>
2020-11-05 23:09:47 +0900

    エラーメッセージを日本語化

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module PostBootstrapApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -31,5 +31,6 @@ module PostBootstrapApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+  
   end
 end
